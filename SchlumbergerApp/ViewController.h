a10bd164e0a41ac88696c9031d6c0466827bca7a
DeviceBee <imran@devicebee.com>
2014-10-02 10:50:25 +0500

    Initial Commit

---
+++ SchlumbergerApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SchlumbergerApp
+//
+//  Created by DeviceBee on 10/2/14.
+//  Copyright (c) 2014 devicebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SchlumbergerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SchlumbergerApp
+//
+//  Created by DeviceBee on 10/2/14.
+//  Copyright (c) 2014 devicebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
